ed6053399640fb2e083cbd820f11194e5fbfe8bb
oleg <legik7127@gmail.com>
2022-06-27 12:11:17 +0300

    solution of ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int numberthreedigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"введено число: {numberthreedigit}");
+if (numberthreedigit <= 99 && numberthreedigit >= -99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра данного числа: {numberthreedigit % 10}");
+}
